251d809d6abc6b27d0a162d2acc65224cbc6f033
guekho64 <guekho64@users.noreply.github.com>
2016-08-31 13:43:13 -0500

    Update notepadqq_es_mx.ts

--- src/translations/notepadqq_es_mx.ts
+++ src/translations/notepadqq_es_mx.ts
@@ -1,3 +1,6 @@
+#Regex to select all '©"Here goes translated text"©': \©\"Here goes translated text\"\©
+
+
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
